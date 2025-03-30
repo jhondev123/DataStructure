97d92e812e13096e1d4b08e4b39b65ef292d98b5
Jhonattan Curtarelli <Jhonattan.curtarelli@outlook.com>
2025-03-30 15:00:17 -0300

    Add project files.

---
+++ Tree/Node.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tree
+{
+    internal class Node
+    {
+        public Node Right;
+        public Node Left;
+        public int Value = 0;
+
+
+    }
+}
---
+++ Tree/Program.cs
@@ -0,0 +1,66 @@
+﻿using Tree;
+Node StartingTree()
+{
+    int[] array = { 45, 20, 35, 8, 55, 99 };
+    /**
+     * 
+     *        45
+     *      /     \
+     *     20      55
+     *    /  \    /  \
+     *   8   35      99
+     */
+    Node node = new Node();
+    node.Value = array[0];
+
+    for (int i = 1; i < array.Length; i++)
+    {
+        CreateNode(node, i);
+
+    }
+
+    void CreateNode(Node node, int value)
+    {
+        Node current;
+        Node temp = new Node();
+
+        temp.Right = null;
+        temp.Left = null;
+        while (true)
+        {
+            current = node;
+            temp.Value = value;
+            if (value < current.Value)
+            {
+                if (node.Left == null)
+                {
+                    node.Left = temp;
+                }
+                else
+                {
+                    CreateNode(node.Left, value);
+                }
+            }
+            else
+            {
+                if (node.Right == null)
+                {
+                    node.Right = temp;
+                }
+                else
+                {
+                    CreateNode(node.Right, value);
+                }
+            }
+            break;
+        }
+    }
+    return node;
+}
+Node node = StartingTree();
+
+Console.WriteLine(node.Value);
+Console.WriteLine(node.Left.Value);
+Console.WriteLine(node.Right.Value);
+Console.WriteLine(node.Left.Value);
+Console.ReadLine();
